e8e8ba2644b3c7c22b4c3be6eed8190cef813b79
Caleb <quantumpandaprog@gmail.com>
2026-07-31 23:01:17 +0300

    feat: add 2 more models

--- src/constants/model.ts
+++ src/constants/model.ts
@@ -1,6 +1,8 @@
 export const MODELS = [
   "openai/gpt-oss-120b",
+  "openai/gpt-oss-20b",
   "llama-3.3-70b-versatile",
+  "llama-3.1-8b-instant",
   "qwen/qwen3-32b",
   "nvidia/nemotron-3-ultra-550b-a55b:free",
   "nex-agi/nex-n2-pro:free",
