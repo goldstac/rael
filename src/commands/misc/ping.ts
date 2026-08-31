fe5c4103963b4088b4e90dff96803592df1ae7f1
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-08-31 17:34:59 +0400

    fix: correct WebSocket casing in ping command

--- src/commands/misc/ping.ts
+++ src/commands/misc/ping.ts
@@ -2,7 +2,7 @@ import type { CommandCallbackOpts } from "../../types/command.ts";
 
 export default {
   name: "ping",
-  description: "Check the bot's latency and websocket ping",
+  description: "Check the bot's latency and WebSocket ping",
   async execute({ client, message }: CommandCallbackOpts) {
     try {
       const ping = Date.now() - message.createdTimestamp;
